d2a7027143ca1c1aebacdcee46553b8e4ad70f8c
Lasexille <47592466+lasex1lle@users.noreply.github.com>
2023-04-16 13:45:44 +0300

    a

--- js/config.js
+++ js/config.js
@@ -1,61 +1,6 @@
-/*    _           _    _                     _ 
-     | |         | |  | |                   | |
-  ___| | ___  ___| | _| |     ___   __ _  __| |
- / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
- \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
- |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
-    version 2.05
-*/
-
-/*URL for the server logo
-Leave it blank to use text instead*/
-var l_serverImage = "images/logo.png";
-
-var l_serverName = "";
-
-var l_bgVideo = false;
-var l_background = "backgrounds/videos/glimmering.webm";
-
-/*(Image-background only)
-Place one or more images
-Multiple images will automatically shuffle between them*/
-var l_bgImages = [
-	"backgrounds/images/1.png",
-	"backgrounds/images/2.jpg",
-	"backgrounds/images/3.jpg",
-	"backgrounds/images/4.jpg",
-	"backgrounds/images/5.jpg"
-];
-
-/*Random background images order?*/
-var l_bgImagesRandom = true;
-
-/*(Image-background only)
-Delay between background images changes
-in milliseconds*/
-var l_bgImageDuration = 5000;
-
-/*(Image-background only)
-Image background fade velocity
-in milliseconds*/
-var l_bgImageFadeVelocity = 2000;
-
-/*Enable background overlay?*/
-var l_bgOverlay = true;
-
-/*Background darkening amount
-0 for none (0%), 100 for pitch black (100%)*/
-var l_bgDarkening = 50;
-
-/*Music player
-'true' or 'false' to enable music in the background*/
 var l_music = true;
-
-/*Display actual song's name?*/
 var l_musicDisplay = true;
 
-/*Music playlist
-Place how much Youtube IDs/.ogg you want*/
 var l_musicPlaylist = [
 	{ogg: "songs/CigaSolitude.mp3", name: ""},
 	{ogg: "songs/CoastTrible.mp3", name: ""},
@@ -64,28 +9,5 @@ var l_musicPlaylist = [
 	{ogg: "songs/StellarWind.mp3", name: ""}
 ];
 
-/*Random music order?*/
 var l_musicRandom = true;
-
-/*Music volume
-Choose a value between 0 and 100*/
 var l_musicVolume = 15;
-
-/*Enable custom messages?*/
-var l_messagesEnabled = true;
-
-/*Enter your custom messages below*/
-var l_messages = [
-"",
-];
-
-/*Random message order?*/
-var l_messagesRandom = false;
-
-/*Delay between message changes
-in milliseconds*/
-var l_messagesDelay = 5000;
-
-/*Messages fade time
-in milliseconds*/
-var l_messagesFade = 1000;
