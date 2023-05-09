efc0a33302d9d177e0c88bbe6964039b9949509d
Lasexille <47592466+lasex1lle@users.noreply.github.com>
2023-05-09 10:35:19 +0300

    9may

--- js/config.js
+++ js/config.js
@@ -2,12 +2,16 @@ var l_music = true;
 var l_musicDisplay = true;
 
 var l_musicPlaylist = [
-	{ogg: "songs/CigaSolitude.mp3", name: ""},
-	{ogg: "songs/CoastTrible.mp3", name: ""},
-	{ogg: "songs/Lifelong.mp3", name: ""},
-	{ogg: "songs/LikeThat.mp3", name: ""},
-	{ogg: "songs/StellarWind.mp3", name: ""},
-	{ogg: "songs/JustBreathing.mp3", name: ""},
+	{ogg: "songs/ww2/10Botalyon.mp3", name: ""},
+	{ogg: "songs/ww2/Smuglyanka.mp3", name: ""},
+	{ogg: "songs/ww2/Zemlyanka.mp3", name: ""},
+
+	// {ogg: "songs/CigaSolitude.mp3", name: ""},
+	// {ogg: "songs/CoastTrible.mp3", name: ""},
+	// {ogg: "songs/Lifelong.mp3", name: ""},
+	// {ogg: "songs/LikeThat.mp3", name: ""},
+	// {ogg: "songs/StellarWind.mp3", name: ""},
+	// {ogg: "songs/JustBreathing.mp3", name: ""},
 ];
 
 var l_musicRandom = true;
